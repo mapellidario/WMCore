e438257d8a5e0a60e4ca589f90a1ed8d27be4ae0
Dario Mapelli <mapelli.dario@gmail.com>
2021-05-17 17:55:45 +0200

    new unittest deploy scripts for wmagentpy3 - debug

--- test/deploy/env_unittest_py3.sh
+++ test/deploy/env_unittest_py3.sh
@@ -4,7 +4,7 @@ export BASE_DIR=$PWD
 export TEST_DIR=$BASE_DIR/wmcore_unittest
 export TEST_SRC=$TEST_DIR/WMCore/src
 export TEST_SRC_PYTHON=$TEST_SRC/python
-export INSTALL_DIR=$BASE_DIR/unittestdeploy/wmagentpy3
+export INSTALL_DIR=$BASE_DIR/unittestdeploy/wmagent
 export ADMIN_DIR=$BASE_DIR
 export CERT_DIR=$BASE_DIR/certs
 
